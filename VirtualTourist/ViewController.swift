eab1e1a1f0777de48e016d9d9b57db2eb31da2cb
ALy Essam <alyhamadah@gmail.com>
2019-09-30 18:42:46 +0200

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Aly Essam on 9/30/19.
+//  Copyright © 2019 Aly Essam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
